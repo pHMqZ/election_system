6698aaf4e86261e1e6defda7c67bd6d2e22f50e4
Phillip Marques <phillipmarq@hotmail.com>
2024-04-13 11:55:07 -0300

    Initial application setup

---
+++ voting-app/src/test/java/me/pms/ElectionSystemIT.java
@@ -0,0 +1,8 @@
+package me.pms;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ElectionSystemIT extends ElectionSystemTest {
+    // Execute the same tests but in packaged mode.
+}
