9a4b4cc196d6306ae9e4b4a53598ed8fe10805ad
Phillip Marques <phillipmarq@hotmail.com>
2024-04-13 17:41:49 -0300

    Developing candidate management services

---
+++ election-management/src/main/java/domain/Candidate.java
@@ -0,0 +1,7 @@
+package domain;
+
+import java.util.Optional;
+
+public record Candidate(String id, Optional<String> photo, String givenName, String familyName,
+                        String email, Optional<String> phone, Optional<String> jobTitle) {
+}
---
+++ election-management/src/main/java/infrastructure/repositories/SQLCandidateRepository.java
@@ -0,0 +1,23 @@
+package infrastructure.repositories;
+
+import domain.Candidate;
+import domain.CandidateQuery;
+import domain.CandidateRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class SQLCandidateRepository implements CandidateRepository {
+    @Override
+    public void save(List<Candidate> candidates) {
+
+    }
+
+    @Override
+    public List<Candidate> find(CandidateQuery query) {
+        return List.of();
+    }
+
+}
