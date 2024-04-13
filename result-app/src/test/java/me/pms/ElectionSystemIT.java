e1c688b3d548a6ada6b051129e0718c674a08638
Phillip Marques <phillipmarq@hotmail.com>
2024-04-13 14:50:11 -0300

    Testing initial setup with quarkus dev

--- result-app/src/test/java/me/pms/ElectionSystemIT.java
+++
@@ -1,8 +0,0 @@
-package me.pms;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ElectionSystemIT extends ElectionSystemTest {
-    // Execute the same tests but in packaged mode.
-}
